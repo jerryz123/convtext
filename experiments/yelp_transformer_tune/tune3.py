5ff51511e969abb1fa7c191f79708dd71dbcbd31
Ubuntu <ubuntu@ip-172-31-0-207.us-west-2.compute.internal>
2018-05-07 10:11:27 +0000

    tunes

--- experiments/yelp_transformer_tune/tune3.py
+++ experiments/yelp_transformer_tune/tune3.py
@@ -4,19 +4,17 @@ from models.text_generator import TransformerGenerator
 DEFAULT_SAVE_DIR = os.path.dirname(os.path.abspath(__file__)) #saved in experiment folder by default
 configuration = {
     'data_dir' : os.path.join(os.environ.get('YELP_DATA_DIR', './'), 'records/'),
-    'batch_size' : 8,
+    'batch_size' : 16,
     'train_val_split' : 0.95, #0.95 by default
     'model' : TransformerGenerator,
-    'model_dir' : os.path.join(os.environ.get('SAVE_DIR', DEFAULT_SAVE_DIR), 'base_model_nodamp/'),
+    'model_dir' : os.path.join(os.environ.get('SAVE_DIR', DEFAULT_SAVE_DIR), 'tune3/'),
     'd_embed' : 512,
-    'n_words' : 7000,
-    'n_title_words' : 7000,
+    'n_words' : 11000,
+    'n_title_words' : 9000,
     'sub_head_dim' : 64,
     'dropout' : 0.25,
-    'feedforward_dim' : 2048,
-    'num_repeats' : 6,
-    'clip_grad' : 1.0,
-    'learning_rate' : 0.0001,
+    'feedforward_dim' : 3096,
+    'num_repeats' : 4,
     'n_iters' : 300000,
     'save_step' : 10000
 }
