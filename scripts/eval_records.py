d631dfab09e68ba4721170a7661cc2bcc74017ef
Sudeep Dasari <sdasari@berkeley.edu>
2018-05-07 23:38:09 -0700

    new eval script

--- scripts/eval_records.py
+++ scripts/eval_records.py
@@ -22,26 +22,49 @@ def token_lookup(tokens, lookup, THRESH):
     
     return words
 
+class WordTranslator:
+    def __init__(self, conf, pkl_path):
+        dicts = pkl.load(open(os.path.join(conf['data_dir'], pkl_path), 'rb'))
+        self.w_lookup = dicts['word_lookup']
+        self.n_lookup = dicts['word_to_token']
+    def tokenize_string(self, string):
+        splits = string.lower().split()
+        return np.array([self.n_lookup[s] for s in splits]).reshape((1, -1))
+    def word_lookup(self, num_list):
+        return [self.w_lookup[s] for s in np.squeeze(num_list)]
+    def num_lookup(self, word_list):
+        return np.array([self.n_lookup[w] for w in word_list]).reshape((1, -1))
 def main():
     model_name = FLAGS.model
     hyperparams = SourceFileLoader('hyperparams', FLAGS.config).load_module()
     conf = hyperparams.configuration
     conf['batch_size'] = 1
 
-    word_lookup_tabel = pkl.load(open(os.path.join(conf['data_dir'], 'word_tabels.pkl'), 'rb'))['word_lookup']
-    title_word_lookup_tabel = pkl.load(open(os.path.join(conf['data_dir'], 'b_name_word_tabels.pkl'), 'rb'))['word_lookup']
+    MAX_GEN = 300
+    TARGET_BUSINESS = "Taco Bell"
+    TARGET_STARS = 5
+
+    print("generating", TARGET_STARS, "review of", TARGET_BUSINESS)
+
+    title_trans = WordTranslator(conf, 'b_name_word_tabels.pkl')
+    text_trans = WordTranslator(conf, 'word_tabels.pkl')
+
+    print(title_trans.tokenize_string(TARGET_BUSINESS))
 
     with tf.variable_scope('model', reuse = None) as training_scope:
-        train_title, train_star, train_text = build_record_reader(conf)
-        train_model = conf['model'](conf, train_title, train_star, train_text)
-        train_model.build()
+        train_title, train_star, train_text, train_valid = build_record_reader(conf)
+        train_model = conf['model'](conf, train_title, train_star, train_text, train_valid)
+        train_model.build(is_Train = True)
 
     with tf.variable_scope('val_model', reuse = None):
-        val_title, val_star, val_text = build_record_reader(conf, isTrain = False)
+        val_title = tf.placeholder(tf.int64, shape = [1, None])
+        val_star = tf.placeholder(tf.int64, shape = [1, 1])
+        val_text = tf.placeholder(tf.int64, shape = [1, None])
+        val_valid = tf.ones_like(val_text, dtype=tf.float32)
 
         with tf.variable_scope(training_scope, reuse=True):
-            val_model = conf['model'](conf, val_title, val_star, val_text)
-            val_model.build()
+            val_model = conf['model'](conf, val_title, val_star, val_text, val_valid)
+            val_model.build(is_Train = False)
 
     optimizer = tf.train.AdamOptimizer()
     gradients, variables = zip(*optimizer.compute_gradients(train_model.loss))
@@ -54,27 +77,14 @@ def main():
     sess = tf.Session(config = config)
     sess.run(tf.global_variables_initializer())
 
-    MODEL_DIR = os.path.join(conf['model_dir'], model_name)
-    print("opening:", MODEL_DIR)
+    MODEL_PATH = os.path.join(conf['model_dir'], model_name)
+    print("opening:", MODEL_PATH)
 
-    saver.restore(sess, MODEL_DIR)   
+    saver.restore(sess, MODEL_PATH)   
+    
+    sentence = ['<START>']
+    while len(sentence) <= 300 and sentence[-1] != '<END>':
 
-    g_truth, logits, loss, input_title, input_stars = sess.run([val_model.input_text, val_model.logits, val_model.loss, val_model.input_title, val_model.input_stars])
-    gen_tokens = np.argmax(logits, axis = 2)
-    
-    print(g_truth.shape, gen_tokens.shape)
-    print('GENERATING REVIEW')
-    print('b_name', ' '.join(token_lookup(input_title[0], title_word_lookup_tabel, conf['n_title_words'])))
-    print('stars', input_stars[0, 0])
-    print()
-    print('GROUND TRUTH VS GEN')
-    g_truth_words, gen_words = token_lookup(g_truth[0], word_lookup_tabel, conf['n_words'] - 1), token_lookup(gen_tokens, word_lookup_tabel, conf['n_words'] - 1)
-    print("REAL")
-    print(' '.join(g_truth_words))
-    print("GEN")
-    print(' '.join(gen_words))
-    
-    
     sess.close()
 
 if __name__ == '__main__':
